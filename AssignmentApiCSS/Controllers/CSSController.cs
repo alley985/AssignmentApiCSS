6d9fbc2007ad0afca9d34c6efc28a06b3ae25b98
YC <YC@DESKTOP-9P2SEAQ>
2022-11-02 10:38:14 +0500

    Add project files.

---
+++ AssignmentApiCSS/Controllers/CSSController.cs
@@ -0,0 +1,22 @@
+﻿using myBookAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace myBookAPI.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class CSSController : ControllerBase
+    {
+        public CSSServices _Services { get; set; }
+        public CSSController(CSSServices cSSServices)
+        {
+            _Services = cSSServices;
+        }
+        [HttpGet]
+        public string Get()
+        {
+            return _Services.getCss();
+        }
+    }
+}
---
+++ AssignmentApiCSS/Services/CSSServices.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace myBookAPI.Services
+{
+    public class CSSServices
+    {
+        public IWebHostEnvironment _environment;
+
+        public CSSServices(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        private string filePath { get {
+                return Path.Combine(_environment.WebRootPath, "css", "MyCSS.css");
+            } }
+       
+
+        public string getCss()
+        {
+            using(var fs = new StreamReader(filePath))
+            {
+                return  fs.ReadToEnd();
+            }
+        }
+
+
+    }
+}
